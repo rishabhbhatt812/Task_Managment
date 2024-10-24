843d774cd2c76810a54adfe2f6d1b2e9da12261d
Rishabh Bhatt <139116378+rishabhbhatt812@users.noreply.github.com>
2024-10-24 20:23:55 +0530

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const conn = async() =>{
   try{
-    const reaponse = await mongoose.connect("mongodb://localhost:27017/Task_Managment");
+    const reaponse = await mongoose.connect("mongodb+srv://rishabhbhatt437:nt7mQNOUwSnHEY3h@cluster0.ljao5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     if(reaponse){
       console.log("Connected to database");
     }
